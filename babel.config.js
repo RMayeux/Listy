8a33896cc5b77a402f0e63c0bfdfce4c76ecac05
Rémi Mayeux <remimayeux@gmail.com>
2021-04-04 02:06:58 +0200

    Fixed npx test bug from dotenv package

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,16 @@
-module.exports = api => {
-  api.cache(true);
-  return {
-    presets: [
-      'module:metro-react-native-babel-preset',
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
       'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
     ],
-  };
+  ],
 };
