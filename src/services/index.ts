dc5dfc68f01272f03f68327c5e5e08b1529b5cd7
Rémi Mayeux <remimayeux@gmail.com>
2021-04-11 01:05:34 +0200

    Fixed error, non existent export

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,5 +3,3 @@ export { default as searchList } from './list/search';
 export { default as deleteList } from './list/delete';
 export { default as createList } from './list/create';
 export { default as updateList } from './list/update';
-
-export { default as searchSharingRequest } from './sharingRequest/search';
